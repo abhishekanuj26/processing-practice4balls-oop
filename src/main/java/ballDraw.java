88abbe0ec62669908cf2898e37b6c0adcbb3537b
Abhishek Anuj <26abhishekanuj@gmail.com>
2022-01-31 21:10:45 +0530

    added the list of objects, ectracted some variables and method

--- src/main/java/ballDraw.java
+++ src/main/java/ballDraw.java
@@ -1,9 +1,9 @@
-public class balls{
+public class ballDraw{
     public static final int Diameter = 10;
     private final int speed;
     private final int height;
     int x=0;
-    public balls(int speed, int height) {
+    public ballDraw(int speed, int height) {
         this.speed = speed;
         this.height = height;
     }
@@ -11,6 +11,9 @@ public class balls{
 
     public void draw() {
         processing4Balls.processing.ellipse(x,height, Diameter,Diameter);
+
+    }
+    public void x_coord_speed() {
         x+=speed;
     }
 
--- src/main/java/processing4Balls.java
+++ src/main/java/processing4Balls.java
@@ -1,9 +1,14 @@
 import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class processing4Balls extends PApplet {
     public static  PApplet processing;
     public static final int HEIGHT = 480;
     public static final int WIDTH = 640;
-
+    int no_of_balls = 4;
+    List<ballDraw> balls=new ArrayList<>(no_of_balls+1);
     public static void main(String[] args) {
         PApplet.main("processing4Balls",args);
     }
@@ -17,21 +22,19 @@ public class processing4Balls extends PApplet {
 
     public void setup() {
         processing=this;
+        int speed=0;
+        for(int i=1;i<=no_of_balls;i++){
+            balls.add(new ballDraw(speed+i,HEIGHT/5*i));
+        }
     }
 
-    balls ball1 = new balls(1, HEIGHT/5);
-    balls ball2 = new balls(2, HEIGHT/5*2);
-    balls ball3 = new balls(3, HEIGHT/5*3);
-    balls ball4 = new balls(4, HEIGHT/5*4);
+
 
     @Override
     public void draw() {
-        ball1.draw();
-
-        ball2.draw();
-
-        ball3.draw();
-
-        ball4.draw();
+        for(int i=0;i<no_of_balls;i++){
+            balls.get(i).draw();
+            balls.get(i).x_coord_speed();
+        }
     }
 }
